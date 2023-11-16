db6ea8fc6fb29b4f23ed9732b33a593b68f1ba4d
João Francisco da Silva <124676444+silvazada@users.noreply.github.com>
2023-11-16 13:25:31 -0200

    Add files via upload

--- bombeirojoao.sql
+++ bombeirojoao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Out-2023 às 13:20
+-- Tempo de geração: 16-Nov-2023 às 16:43
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -28,47 +28,51 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `ocorrencia` (
-  `data` date NOT NULL,
-  `sexo` varchar(2) NOT NULL,
+  `id_ocorrencia` int(11) NOT NULL,
+  `data_ocorrencia` date NOT NULL,
+  `sexo` varchar(1) NOT NULL,
   `nome_hospital` varchar(255) NOT NULL,
   `nome_paciente` varchar(255) NOT NULL,
+  `registro_paciente` varchar(15) NOT NULL,
   `idade_paciente` int(11) NOT NULL,
-  `registro_paciente` int(15) NOT NULL,
-  `telefone_paciente` int(11) NOT NULL,
+  `telefone_paciente` varchar(11) NOT NULL,
   `local_ocorrencia` varchar(255) NOT NULL,
   `nome_acompanhante` varchar(255) DEFAULT NULL,
-  `idade_acompanhante` int(11) DEFAULT NULL
+  `idade_acompanhante` int(11) DEFAULT NULL,
+  `numero_usb` varchar(255) DEFAULT NULL,
+  `desp` varchar(255) DEFAULT NULL,
+  `h_ch` varchar(255) DEFAULT NULL,
+  `km_final` varchar(255) DEFAULT NULL,
+  `cod_ir` varchar(4) DEFAULT NULL,
+  `cod_ps` varchar(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estrutura da tabela `tipo_ocorrencia`
+-- Extraindo dados da tabela `ocorrencia`
 --
 
-CREATE TABLE `tipo_ocorrencia` (
-  `causado_animais` int(11) DEFAULT NULL,
-  `meio_transporte` int(11) DEFAULT NULL,
-  `desmoronamento/deslizamento` int(11) DEFAULT NULL,
-  `emergencia_medica` int(11) DEFAULT NULL,
-  `queda_2M` int(11) DEFAULT NULL,
-  `tentativa_suicidio` int(11) DEFAULT NULL,
-  `queda_propria_altura` int(11) DEFAULT NULL,
-  `afogamento` int(11) DEFAULT NULL,
-  `agressao` int(11) DEFAULT NULL,
-  `atropelamento` int(11) DEFAULT NULL,
-  `choque_eletrico` int(11) DEFAULT NULL,
-  `desabamento` int(11) DEFAULT NULL,
-  `domestico` int(11) DEFAULT NULL,
-  `esportivo` int(11) DEFAULT NULL,
-  `intoxicacao` int(11) DEFAULT NULL,
-  `queda_bicicleta` int(11) DEFAULT NULL,
-  `queda_moto` int(11) DEFAULT NULL,
-  `queda_menor_2M` int(11) DEFAULT NULL,
-  `trabalho` int(11) DEFAULT NULL,
-  `transferencia` int(11) DEFAULT NULL,
-  `outro` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+INSERT INTO `ocorrencia` (`id_ocorrencia`, `data_ocorrencia`, `sexo`, `nome_hospital`, `nome_paciente`, `registro_paciente`, `idade_paciente`, `telefone_paciente`, `local_ocorrencia`, `nome_acompanhante`, `idade_acompanhante`, `numero_usb`, `desp`, `h_ch`, `km_final`, `cod_ir`, `cod_ps`) VALUES
+(1, '2023-11-02', 'm', 'Dona Helena', 'Manuel Arduino da Ferreira', '8094023', 54, '47988152574', 'Av. Brasil', NULL, NULL, '', '', '', '', NULL, NULL);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `ocorrencia`
+--
+ALTER TABLE `ocorrencia`
+  ADD PRIMARY KEY (`id_ocorrencia`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `ocorrencia`
+--
+ALTER TABLE `ocorrencia`
+  MODIFY `id_ocorrencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
